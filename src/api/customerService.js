171cacfb8dd1cc1b61b0643309ee33aacf201f40
Manoj Bhaskar <130078620+saimanoj1437@users.noreply.github.com>
2024-11-16 23:39:28 +0530

    Update customerService.js

--- src/api/customerService.js
+++ src/api/customerService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const createCustomer = async (customerData) => {
     try {
-        const response = await axios.post('http://localhost:5000/api/customers/add', customerData);
+        const response = await axios.post('https://xenobackend-production.up.railway.app/api/customers/add', customerData);
         return response.data;
     } catch (error) {
         console.error("Failed to create customer:", error);
