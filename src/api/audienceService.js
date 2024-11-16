8a9d1e5c302bd6e7d97a045cda9e5fb55e54a8ba
Manoj Bhaskar <130078620+saimanoj1437@users.noreply.github.com>
2024-11-16 23:39:04 +0530

    Update audienceService.js

--- src/api/audienceService.js
+++ src/api/audienceService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const createAudienceSegment = async (conditions) => {
     try {
-        const response = await axios.post('http://localhost:5000/api/audiences', { conditions });
+        const response = await axios.post('https://xenobackend-production.up.railway.app/api/audiences', { conditions });
         return response.data;
     } catch (error) {
         console.error("Error creating audience:", error);
