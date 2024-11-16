200fb151a61d9ef252eaecc76c6623632d481af9
Manoj Bhaskar <130078620+saimanoj1437@users.noreply.github.com>
2024-11-16 23:24:51 +0530

    Update LoginForm.js

--- src/pages/LoginForm.js
+++ src/pages/LoginForm.js
@@ -18,7 +18,7 @@ const LoginForm = () => {
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:5000/api/auth/login', { email, password });
+            const response = await axios.post('https://xenobackend-production.up.railway.app/api/auth/login', { email, password });
             localStorage.setItem('token', response.data.token);
             navigate('/dashboard');
         } catch (err) {
